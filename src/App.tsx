3d2e85d99cc8f17aa1e1938c783fee2d3564e1a0
lirenxn <lirenxn@gmail.com>
2022-07-10 20:03:06 +1000

    feat(samples): add samples

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 import "./styles.css";
 import { from, of, delay, map, concatMap } from "rxjs";
 import StreamPrinter from "./components/StreamPrinter";
+import { getStreams } from "./samples/streams";
 
-const stream$ = from([1, 2, 3]).pipe(
-  map((x) => x * x),
-  concatMap((x) => of(x).pipe(delay(500)))
-);
-
+const { stream1$, stream2$, stream3$, stream4$ } = getStreams();
 export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <StreamPrinter stream$={stream$} />
+      <StreamPrinter stream$={stream1$} />
     </div>
   );
 }
--- src/samples/streams.tsx
+++ src/samples/streams.tsx
@@ -1,15 +1,29 @@
-import { concatMap, delay, from, of } from "rxjs";
-import { myStartWith } from "../custom-pipeable-operator/common";
+import { from, of, delay, concatMap } from "rxjs";
 
-export const stream2$ = from(["a", "b", "c"]).pipe(
-  concatMap((x) => of(x).pipe(delay(600)))
-);
+enum UNIT {
+  _SEC = 1000,
+  _500MS = 500,
+  _100MS = 100
+}
 
-export const stream3$ = from(["🤔", "💪🏻", "✅"]).pipe(
-  concatMap((x) => of(x).pipe(delay(100)))
-);
-
-export const stream4$ = from([<b>hi</b>, <b>go</b>, <b>to</b>]).pipe(
-  concatMap((x) => of(x).pipe(delay(300))),
-  myStartWith("!START!")
-);
+export function getStreams(unit: UNIT = UNIT._100MS) {
+  const streams$ = {
+    // 1 sec interval
+    stream1$: from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]).pipe(
+      concatMap((x) => of(x).pipe(delay(1 * unit)))
+    ),
+    // 2 sec interval
+    stream2$: from(["🤔", "💪🏻", "✅", "👍", "📈"]).pipe(
+      concatMap((x) => of(x).pipe(delay(2 * unit)))
+    ),
+    // 3 sec interval
+    stream3$: from(["{||}", "<||>", "[||]", "||/"]).pipe(
+      concatMap((x) => of(x).pipe(delay(3 * unit)))
+    ),
+    // 4 sec interval
+    stream4$: from(["XX", "YY", "ZZ"]).pipe(
+      concatMap((x) => of(x).pipe(delay(4 * unit)))
+    )
+  };
+  return streams$;
+}
