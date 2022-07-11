9ae3c5acf4f098c01f62f13a8f48c924a0d61187
lirenxn <lirenxn@gmail.com>
2022-07-11 19:20:07 +1000

    misc(stream printer): improve StreamPrinter

--- src/components/StreamPrinter.tsx
+++ src/components/StreamPrinter.tsx
@@ -1,6 +1,11 @@
 import "./styles.css";
-import { ReactNode, useEffect, useState } from "react";
+import { ReactNode, useEffect, useRef, useState } from "react";
 import { map, Observable, startWith } from "rxjs";
+
+function getDifference(now: Date, start: Date) {
+  const difference = String(now.valueOf() - start.valueOf()).padStart(4, "0");
+  return `${difference.slice(0, -3)}.${difference.slice(-3)}s`;
+}
 
 export default function StreamPrinter({
   stream$,
@@ -17,27 +22,27 @@ export default function StreamPrinter({
     setOutputList((prev) => [...prev, newLine]);
   };
 
+  const startRef = useRef<undefined | Date>();
+
   useEffect(() => {
-    let start: undefined | Date;
+    startRef.current = undefined;
     const subscription = stream$
       .pipe(
         startWith(starter),
         map((node) => {
           const now = new Date();
-          start = start === undefined ? now : start;
-          const difference = String(now.valueOf() - start.valueOf()).padStart(
-            4,
-            "0"
-          );
+          startRef.current =
+            startRef.current === undefined ? now : startRef.current;
+          const difference = getDifference(now, startRef.current);
           return (
             <>
               <span className="stream-printer-event atom-block">{node}</span> at{" "}
               <code className="stream-printer-sec-stamp atom-block">
-                {difference.slice(0, -3)}.{difference.slice(-3)}s
+                {difference}
               </code>
               {verbose && (
                 <>
-                  <p>Start: {start.toISOString()}</p>
+                  <p>Start: {startRef.current.toISOString()}</p>
                   <p>Now: {now.toISOString()}</p>
                 </>
               )}
@@ -47,14 +52,54 @@ export default function StreamPrinter({
       )
       .subscribe({
         next: (ev: ReactNode) => println(ev),
-        error: (e) =>
+        error: (e) => {
+          const now = new Date();
           println(
-            <div className="stream-printer-error">
-              {e.message || e.toString()}
+            <div className="stream-printer-meta-line stream-printer-error">
+              {e.message || e.toString()} at
+              <code className="stream-printer-sec-stamp atom-block">
+                {startRef.current
+                  ? getDifference(new Date(), startRef.current)
+                  : "Not events"}
+              </code>
+              {verbose && (
+                <>
+                  <p>
+                    Start:{" "}
+                    {startRef.current
+                      ? startRef.current.toISOString()
+                      : "Not events"}
+                  </p>
+                  <p>Now: {now.toISOString()}</p>
+                </>
+              )}
             </div>
-          ),
-        complete: () =>
-          println(<div className="stream-printer-complete">DONE</div>)
+          );
+        },
+        complete: () => {
+          const now = new Date();
+          println(
+            <div className="stream-printer-meta-line stream-printer-complete">
+              DONE at
+              <code className="stream-printer-sec-stamp atom-block">
+                {startRef.current
+                  ? getDifference(new Date(), startRef.current)
+                  : "Not events"}
+              </code>
+              {verbose && (
+                <>
+                  <p>
+                    Start:{" "}
+                    {startRef.current
+                      ? startRef.current.toISOString()
+                      : "Not events"}
+                  </p>
+                  <p>Now: {now.toISOString()}</p>
+                </>
+              )}
+            </div>
+          );
+        }
       });
     return () => subscription.unsubscribe();
   }, []);
