78751865adbe5d07461102ef2ef2efde1346b53a
lirenxn <lirenxn@gmail.com>
2022-07-11 19:25:53 +1000

    misc(App): modify App

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,17 @@ import {
   myCombineLatestWith,
   myMergeWith
 } from "./custom-pipeable-operator/common";
+import { combineLatestWith, zipWith } from "rxjs";
 
 const { stream1$, stream2$, stream3$, stream4$ } = getStreams();
-const merged$ = stream1$.pipe(myMergeWith(stream2$, stream3$, stream4$));
+const merged$ = stream1$.pipe(zipWith(stream2$, stream3$, stream4$));
 
 export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <StreamPrinter stream$={merged$} />
+      {/* <StreamPrinter stream$={merged$} /> */}
+      <StreamPrinter stream$={stream4$} />
     </div>
   );
 }
