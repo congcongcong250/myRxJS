4a835cc4e297fda2c1dc71d0850fe5a6c4bb9aea
lirenxn <lirenxn@gmail.com>
2022-07-11 19:26:37 +1000

    fix(myZipWith): use groupComplete for myZipWith

--- src/custom-pipeable-operator/common.ts
+++ src/custom-pipeable-operator/common.ts
@@ -203,9 +203,14 @@ export function myZipWith(...streams$: Observable<any>[]) {
         createValueCache(allStreams$)
       ];
       const groupSubscription = new GroupSubscription();
+      const groupComplete = createGroupComplete(
+        allStreams$.length,
+        observer.complete.bind(observer)
+      );
       allStreams$.forEach((s$, i) => {
         const inObserver = {
           ...forwardObserver(observer),
+          complete: groupComplete,
           next: (v: any) => {
             /**
              * Not very pure, not very functional
